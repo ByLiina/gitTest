32cdaecf999dbe4d74c2a538cae05b6afca7e7c4
ByLiina <bistrova.lina@inbox.lv>
2024-03-06 13:53:35 +0200

    NewCommit06.03

---
+++ Assets/Scripts/CollectCoins.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoins : MonoBehaviour
+{
+    [Tooltip("The particles that appear after the player collects a coin.")]
+    public GameObject coinParticles;
+
+    private int coins;
+
+    PlayerMovement playerMovementScript;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerMovementScript = other.GetComponent<PlayerMovement>();
+            playerMovementScript.soundManager.PlayCoinSound();
+            coins++;
+            if (coins % 10 == 1)
+            {
+                playerMovementScript.playerStats.ChangeHealth(+10f);
+            }
+            playerMovementScript.playerStats.ChangeHealth(+10f);
+            ScoreManager.score += 10;
+            GameObject particles = Instantiate(coinParticles, transform.position, new Quaternion());
+            Destroy(gameObject);
+        }
+    }
+}
